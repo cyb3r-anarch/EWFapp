2884d4683bd66f0a063a8b5d257f7768a37def03
cyber-anarch <aanon408@gmail.com>
2020-08-08 14:25:39 -0700

    Changes to be committed:

---
+++ EWF/Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace rock_paper_scissors
+{
+    partial class rpsApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(rpsApp));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtCountdown = new System.Windows.Forms.Label();
+            this.txtRounds = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.countDownTimer = new System.Windows.Forms.Timer(this.components);
+            this.picCpu = new System.Windows.Forms.PictureBox();
+            this.picPlayer = new System.Windows.Forms.PictureBox();
+            this.btnEarth = new System.Windows.Forms.Button();
+            this.btnWater = new System.Windows.Forms.Button();
+            this.btnFire = new System.Windows.Forms.Button();
+            this.txtScore = new System.Windows.Forms.Label();
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCpu)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPlayer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkKhaki;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 35);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(773, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(27, 27);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // txtCountdown
+            // 
+            this.txtCountdown.AutoSize = true;
+            this.txtCountdown.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCountdown.Location = new System.Drawing.Point(423, 201);
+            this.txtCountdown.Name = "txtCountdown";
+            this.txtCountdown.Size = new System.Drawing.Size(24, 26);
+            this.txtCountdown.TabIndex = 4;
+            this.txtCountdown.Text = "5";
+            this.txtCountdown.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtRounds
+            // 
+            this.txtRounds.AutoSize = true;
+            this.txtRounds.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRounds.Location = new System.Drawing.Point(382, 387);
+            this.txtRounds.Name = "txtRounds";
+            this.txtRounds.Size = new System.Drawing.Size(97, 26);
+            this.txtRounds.TabIndex = 5;
+            this.txtRounds.Text = "Round: 3";
+            this.txtRounds.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(220, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Player";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(600, 103);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "CPU";
+            // 
+            // countDownTimer
+            // 
+            this.countDownTimer.Interval = 1000;
+            this.countDownTimer.Tick += new System.EventHandler(this.countDownTimerEvent);
+            // 
+            // picCpu
+            // 
+            this.picCpu.Image = global::rock_paper_scissors.Properties.Resources.unknown;
+            this.picCpu.Location = new System.Drawing.Point(522, 125);
+            this.picCpu.Name = "picCpu";
+            this.picCpu.Size = new System.Drawing.Size(197, 167);
+            this.picCpu.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picCpu.TabIndex = 2;
+            this.picCpu.TabStop = false;
+            // 
+            // picPlayer
+            // 
+            this.picPlayer.Enabled = false;
+            this.picPlayer.Image = global::rock_paper_scissors.Properties.Resources.unknown;
+            this.picPlayer.Location = new System.Drawing.Point(147, 125);
+            this.picPlayer.Name = "picPlayer";
+            this.picPlayer.Size = new System.Drawing.Size(188, 167);
+            this.picPlayer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picPlayer.TabIndex = 2;
+            this.picPlayer.TabStop = false;
+            // 
+            // btnEarth
+            // 
+            this.btnEarth.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnEarth.Location = new System.Drawing.Point(12, 115);
+            this.btnEarth.Name = "btnEarth";
+            this.btnEarth.Size = new System.Drawing.Size(114, 32);
+            this.btnEarth.TabIndex = 8;
+            this.btnEarth.Text = "Earth";
+            this.btnEarth.UseVisualStyleBackColor = true;
+            this.btnEarth.Click += new System.EventHandler(this.btnEarth_Click);
+            // 
+            // btnWater
+            // 
+            this.btnWater.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnWater.Location = new System.Drawing.Point(12, 233);
+            this.btnWater.Name = "btnWater";
+            this.btnWater.Size = new System.Drawing.Size(114, 35);
+            this.btnWater.TabIndex = 9;
+            this.btnWater.Text = "Water";
+            this.btnWater.UseVisualStyleBackColor = true;
+            this.btnWater.Click += new System.EventHandler(this.btnWater_Click);
+            // 
+            // btnFire
+            // 
+            this.btnFire.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnFire.Location = new System.Drawing.Point(11, 387);
+            this.btnFire.Name = "btnFire";
+            this.btnFire.Size = new System.Drawing.Size(115, 32);
+            this.btnFire.TabIndex = 10;
+            this.btnFire.Text = "Fire";
+            this.btnFire.UseVisualStyleBackColor = true;
+            this.btnFire.Click += new System.EventHandler(this.btnFire_Click);
+            // 
+            // txtScore
+            // 
+            this.txtScore.AutoSize = true;
+            this.txtScore.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtScore.Location = new System.Drawing.Point(356, 125);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(151, 22);
+            this.txtScore.TabIndex = 11;
+            this.txtScore.Text = "Player: 0 - CPU: 0";
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnRestart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRestart.Location = new System.Drawing.Point(568, 387);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(125, 32);
+            this.btnRestart.TabIndex = 12;
+            this.btnRestart.Text = "Restart";
+            this.btnRestart.UseVisualStyleBackColor = true;
+            this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label1.Location = new System.Drawing.Point(356, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 20);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Developed by Me.";
+            // 
+            // rpsApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnRestart);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.btnFire);
+            this.Controls.Add(this.btnWater);
+            this.Controls.Add(this.btnEarth);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtRounds);
+            this.Controls.Add(this.txtCountdown);
+            this.Controls.Add(this.picCpu);
+            this.Controls.Add(this.picPlayer);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "rpsApp";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.rpsApp_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picCpu)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPlayer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox picPlayer;
+        private System.Windows.Forms.PictureBox picCpu;
+        private System.Windows.Forms.Label txtCountdown;
+        private System.Windows.Forms.Label txtRounds;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Timer countDownTimer;
+        private System.Windows.Forms.Button btnEarth;
+        private System.Windows.Forms.Button btnWater;
+        private System.Windows.Forms.Button btnFire;
+        private System.Windows.Forms.Label txtScore;
+        private System.Windows.Forms.Button btnRestart;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ EWF/Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rock_paper_scissors
+{
+    public partial class rpsApp : Form
+    {
+
+
+        int rounds = 3;
+        int timerPerRound = 6;
+        bool gameOver = false;
+
+        string[] CPUchoiceList = { "fire", "water", "earth","fire", "water", "earth" };
+        int randomNumber = 0;
+        Random rnd = new Random();
+
+        string CPUchoice;
+        string playerChoice;
+        int playerScore;
+        int CPUScore;
+
+        public rpsApp()
+        {
+            InitializeComponent();
+            countDownTimer.Enabled = true;
+            playerChoice = "none";
+            txtCountdown.Text = "5";
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void rpsApp_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void countDownTimerEvent(object sender, EventArgs e)
+        {
+            timerPerRound -= 1;
+            txtCountdown.Text = timerPerRound.ToString();
+            if (timerPerRound < 1)
+            {
+                countDownTimer.Enabled = false;
+                timerPerRound = 6;
+                randomNumber = rnd.Next(0, CPUchoiceList.Length);
+                CPUchoice = CPUchoiceList[randomNumber];
+                switch (CPUchoice)
+                {
+                    case "earth":
+                        picCpu.Image = Properties.Resources.Rock;
+
+                        break;
+                    case "fire":
+                        picCpu.Image = Properties.Resources.fire;
+
+                        break;
+                    case "water":
+                        picCpu.Image = Properties.Resources.water;
+
+                        break;
+                }
+                if (rounds > 0)
+                {
+                    checkGame();
+                }
+                else
+                {
+                    if (playerScore > CPUScore)
+                    {
+                        MessageBox.Show("You are the Chose One!");
+                    }
+                    else
+                    {
+                        MessageBox.Show("You aren't ready yet to save the world.");
+                    }
+                    gameOver = true;
+                }
+            }
+
+            
+            
+            txtRounds.Text = "Round: " + rounds;
+
+        }
+
+        private void btnEarth_Click(object sender, EventArgs e)
+        {
+            picPlayer.Image = Properties.Resources.Rock;
+            playerChoice = "earth";
+        }
+
+        private void btnWater_Click(object sender, EventArgs e)
+        {
+            picPlayer.Image = Properties.Resources.water;
+            picPlayer.Image.RotateFlip(RotateFlipType.RotateNoneFlipX);
+            picPlayer.Invalidate();
+            playerChoice = "water";
+        }
+
+        private void btnFire_Click(object sender, EventArgs e)
+        {
+            picPlayer.Image = Properties.Resources.fire;
+            picPlayer.Image.RotateFlip(RotateFlipType.RotateNoneFlipX);
+            playerChoice = "fire";
+        }
+        private void checkGame()
+        {
+            if (playerChoice  == "earth" && CPUchoice == "water")
+            {
+                CPUScore += 1;
+                rounds -= 1;
+                MessageBox.Show("Cpu wins, they water bended your blood and killed you!");
+            }
+            else if (playerChoice == "water" && CPUchoice == "earth")
+            {
+                playerScore += 1;
+                rounds -= 1;
+                MessageBox.Show("You win, you water bended their blood and killed them!");
+            }
+            else if (playerChoice == "fire" && CPUchoice == "water")
+            {
+                playerScore += 1;
+                rounds -= 1;
+                MessageBox.Show("You win, their water bending skills were no match to your fire bending skills that easily evaporated them!");
+            }
+            else if (playerChoice == "water" && CPUchoice == "fire")
+            {
+                CPUScore += 1;
+                rounds -= 1;
+                MessageBox.Show("Cpu win, your water bending skills were no match to their fire bending skills and easily evaporated you!");
+            }
+            else if (playerChoice == "earth" && CPUchoice == "fire")
+            {
+                playerScore += 1;
+                rounds -= 1;
+                MessageBox.Show("You win, they tried but tis but a scratch!");
+            }
+            else if (playerChoice == "fire" && CPUchoice == "earth")
+            {
+                CPUScore += 1;
+                rounds -= 1;
+                MessageBox.Show("They win, your fire was too weak to stop those chuncky rocks!");
+            }
+            else if (playerChoice == "water" && CPUchoice == "water")
+            {
+                MessageBox.Show("you both are at the same skill level with this element!");
+            }
+            else if (playerChoice == "fire" && CPUchoice == "fire")
+            {
+                MessageBox.Show("you both are at the same skill level with this element!");
+            }
+            else if (playerChoice == "earth" && CPUchoice == "earth")
+            {
+                MessageBox.Show("you both are at the same skill level with this element!");
+            }
+            else
+            {
+                MessageBox.Show("You need to make a choice!");
+            }
+            startNextRound();
+
+
+
+
+        }
+        
+        private void startNextRound()
+        {
+            if (gameOver == true)
+            {
+                return;
+            }
+            else
+            {
+             
+            }
+            txtScore.Text = $"Player:{playerScore} CPU:{CPUScore}";
+            playerChoice = "none";
+
+            countDownTimer.Enabled = true;
+            picPlayer.Image = Properties.Resources.unknown;
+            picCpu.Image = Properties.Resources.unknown;
+        }
+
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+            playerScore = 0;
+            CPUScore = 0;
+            rounds = 3;
+            txtScore.Text = $"Player:{playerScore} CPU:{CPUScore}";
+            playerChoice = "none";
+            timerPerRound = 6;
+            countDownTimer.Enabled = true;
+            txtCountdown.Text = timerPerRound.ToString();
+            picPlayer.Image = Properties.Resources.unknown;
+            picCpu.Image = Properties.Resources.unknown;
+            gameOver = false;
+        }
+    }
+}
